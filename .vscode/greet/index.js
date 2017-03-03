be4965572987faa4c7e5dc5c63519876aee1ceab
Scott Davis <odogulu@ucsc.edu>
2017-03-02 22:36:08 -0800

    module patters

---
+++ .vscode/greet/index.js
@@ -0,0 +1,7 @@
+var english = require('./english1');
+var spanish = require('./spanish');
+
+module.exports = {
+english: english,
+spanish: spanish
+}
--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-var name = 'Jane';
+var greet = require('./greet');
 
-(function(lastname){
-var name = 'Scott';
-console.log(name);
-console.log(lastname);
-}('Davis'));// immidietly invoke function impression
-
-console.log(name);
+greet.english();
+greet.spanish();
